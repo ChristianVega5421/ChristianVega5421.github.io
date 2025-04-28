76df94033524ed24b738a209c2ba1def4447e7a4
ChristianVega5421 <steeper.5421@gmail.com>
2025-04-27 22:46:37 -0500

    base

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,6 @@ export default defineConfig({
     server: {
         // allowedHosts: ['127.0.0.1', 'glowing-rich-locust.ngrok-free.app'],
     },
-    // base:"https://christianvega5421.github.io/",
+    base:"https://christianvega5421.github.io/",
     plugins: [react()],
 })
